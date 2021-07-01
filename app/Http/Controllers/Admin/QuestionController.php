0de89c9d3f02113258dc09fe760d64ed500b3ffa
João G. Silva <joogabrielsntn66@gmail.com>
2021-07-01 14:49:41 -0400

    Ajustando imports

--- app/Http/Controllers/Admin/QuestionController.php
+++ app/Http/Controllers/Admin/QuestionController.php
@@ -3,9 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Question\DeleteQuestionRequest;
-use App\Http\Requests\Question\StoreQuestionRequest;
-use App\Http\Requests\Question\UpdateQuestionRequest;
+use App\Http\Requests\Question\{DeleteQuestionRequest, StoreQuestionRequest, UpdateQuestionRequest};
 use App\Models\Question;
 use App\Http\Resources\QuestionResource;
 use Illuminate\Support\Facades\Log;
