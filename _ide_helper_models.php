781a799977036b10963049d14a4f5126b1155eda
João G. Silva <39093969+Ikamihiro@users.noreply.github.com>
2021-07-01 09:15:19 -0400

    Feat/change sanctum to jwt (#1)
    * Removendo Sanctum para usar JWT
    
    * Adicionando JWT e configurando-o
    
    * Ajustando as rotas de login e register
    
    * Adicionando novas rotas de Auth

--- _ide_helper_models.php
+++ _ide_helper_models.php
@@ -81,8 +81,6 @@ namespace App\Models{
  * @property string|null $deleted_at
  * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
  * @property-read int|null $notifications_count
- * @property-read \Illuminate\Database\Eloquent\Collection|\Laravel\Sanctum\PersonalAccessToken[] $tokens
- * @property-read int|null $tokens_count
  * @method static \Database\Factories\UserFactory factory(...$parameters)
  * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
@@ -97,6 +95,6 @@ namespace App\Models{
  * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
  */
-	class User extends \Eloquent {}
+	class User extends \Eloquent implements \Tymon\JWTAuth\Contracts\JWTSubject {}
 }
 
--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -6,55 +6,52 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use App\Http\Requests\Auth\UserRegisterRequest;
 use App\Models\User;
-use Carbon\Carbon;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     public function register(UserRegisterRequest $request)
     {
-        DB::beginTransaction();
-
-        try {
-            $user = User::create($request->validated());
-
-            // TODO: Criar o Profile com pontuação zerada
-
-            DB::commit();
-        } catch (\Throwable $th) {
-            DB::rollBack();
-
-            throw $th;
-        }
+        $user = User::create($request->validated());
 
         return response()->json([
             'user' => $user,
-            'access_token' => $user->createToken($request->name)->plainTextToken,
         ], 201);
     }
 
     public function login(LoginRequest $request)
     {
-        $user = User::where('email', $request->email)->first();
-
-        if (!$user || !Hash::check($request->password, $user->password)) {
-            throw ValidationException::withMessages([
-                'email' => ['The provided credentials are incorrect.'],
-            ]);
+        if (!$token = Auth::attempt($request->validated())) {
+            return response()->json(['error' => 'Unauthorized'], 401);
         }
 
-        $user->tokens()->delete();
-
         return response()->json([
-            'user' => $user,
-            'access_token' => $user->createToken($user->email)->plainTextToken,
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => Auth::factory()->getTTL() * 60,
         ]);
     }
 
     public function getCurrentUser()
     {
         return response()->json(auth()->user());
+    }
+
+    public function refresh()
+    {
+        $token = Auth::refresh();
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => Auth::factory()->getTTL() * 60,
+        ]);
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+
+        return response()->noContent();
     }
 }
