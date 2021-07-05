fa3d0d3b199f1ff237b73ec5aaf399e093096968
João G. Silva <joogabrielsntn66@gmail.com>
2021-07-05 08:16:33 -0400

    Criada model que representa a Avaliação

---
+++ app/Models/Evaluation.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\Uuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Evaluation extends Model
+{
+    use HasFactory, Uuids, SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'questionnaire_id',
+    ];
+
+    protected $casts = [
+        'finished_at' => 'datetime',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function questionnaire()
+    {
+        return $this->belongsTo(Questionnaire::class);
+    }
+
+    public function finish()
+    {
+        $this->update([
+            'finished_at' => now(),
+        ]);
+    }
+}
---
+++ database/migrations/2021_07_05_120921_create_evaluations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEvaluationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('evaluations', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('user_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignUuid('questionnaire_id')->constrained('questionnaires')->cascadeOnDelete();
+            $table->timestamp('finished_at')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('evaluations');
+    }
+}
