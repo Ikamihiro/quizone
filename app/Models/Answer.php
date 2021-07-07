46cb8948fce20e1aadd0a6b20d5f47a8d8c1da23
João G. Silva <joogabrielsntn66@gmail.com>
2021-07-07 18:00:20 -0400

    Criada model para representar as Repostas

---
+++ app/Models/Answer.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\Uuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Answer extends Model
+{
+    use HasFactory, Uuids, SoftDeletes;
+
+    protected $fillable = [
+        'evaluation_id',
+        'question_id',
+        'option_id',
+        'correct',
+    ];
+
+    public function evaluation()
+    {
+        return $this->belongsTo(Evaluation::class);
+    }
+
+    public function question()
+    {
+        return $this->belongsTo(Question::class);
+    }
+
+    public function option()
+    {
+        return $this->belongsTo(Option::class);
+    }
+}
---
+++ database/migrations/2021_07_07_212646_create_answers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnswersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('evaluation_id')->constrained('evaluations')->cascadeOnDelete();
+            $table->foreignUuid('question_id')->constrained('questions')->cascadeOnDelete();
+            $table->foreignUuid('option_id')->constrained('options')->cascadeOnDelete();
+            $table->boolean('correct');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('answers');
+    }
+}
